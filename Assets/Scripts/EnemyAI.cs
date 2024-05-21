86ba1f63bfe4626aad27862c838fd39f7dc6975d
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-21 00:23:36 -0600

    Draw wire to show enemy aggro range

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,7 +6,10 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
   [SerializeField] Transform target;
+  [SerializeField] float chaseRange = 10f;
+
   NavMeshAgent navMeshAgent;
+  float distanceToTarget = Mathf.Infinity;
 
   void Start()
   {
@@ -15,6 +18,16 @@ public class EnemyAI : MonoBehaviour
 
   void Update()
   {
-    navMeshAgent.SetDestination(target.position);
+    distanceToTarget = Vector3.Distance(target.position, transform.position);
+    if (distanceToTarget <= chaseRange)
+    {
+      navMeshAgent.SetDestination(target.position);
+    }
+  }
+
+  void OnDrawGizmosSelected()
+  {
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireSphere(transform.position, chaseRange);
   }
 }
