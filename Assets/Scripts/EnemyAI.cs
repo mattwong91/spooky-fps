e56bd8ae146094d21ab7e0e14b02458ca3deabbc
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-23 01:29:40 -0600

    Change animations through code

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -45,12 +45,14 @@ public class EnemyAI : MonoBehaviour
 
   void ChaseTarget()
   {
+    GetComponent<Animator>().SetBool("attack", false);
+    GetComponent<Animator>().SetTrigger("move");
     navMeshAgent.SetDestination(target.position);
   }
 
   void AttackTarget()
   {
-    Debug.Log("Enemy is attacking");
+    GetComponent<Animator>().SetBool("attack", true);
   }
 
   void OnDrawGizmosSelected()
