f9da0a3ccdbea80b07cc805ff8992b020c93e847
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-23 01:59:28 -0600

    Player takes damage

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-  [SerializeField] Transform target;
+  PlayerHealth target;
   [SerializeField] float damage = 40f;
 
   void Start()
   {
-
+    target = FindObjectOfType<PlayerHealth>();
   }
 
   public void AttackHitEvent()
   {
     if (target == null) { return; }
     Debug.Log("Enemy hit you");
+    target.TakeDamage(damage);
   }
 
 }
