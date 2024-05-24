9352f7952ff28f03f7013241268c83e0e3c24104
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-23 22:07:51 -0600

    UI triggered on playerdeath

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -8,6 +8,9 @@ public class SceneLoader : MonoBehaviour
   public void ReloadGame()
   {
     SceneManager.LoadScene(0);
+    Time.timeScale = 1;
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
   }
 
   public void QuitGame()
