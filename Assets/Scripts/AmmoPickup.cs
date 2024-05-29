14afe2de6e9860d4040fa91c6b704c7b5c87d937
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-29 02:28:57 -0600

    Ammo pickups implemented

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
+  [SerializeField] int ammoAmount = 5;
+  [SerializeField] AmmoType ammoType;
+
   void OnTriggerEnter(Collider other)
   {
     if (other.gameObject.tag == "Player")
     {
-      Debug.Log("You got the pickup! - " + name);
+      FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
       Destroy(gameObject);
     }
   }
