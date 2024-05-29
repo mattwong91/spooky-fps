2fb8d4bf7e2d848498624f9cf6c5ecfa61e7b23b
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-29 03:11:30 -0600

    Add enemy animations

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -6,13 +6,27 @@ public class EnemyHealth : MonoBehaviour
 {
   [SerializeField] float hitPoints = 100f;
 
+  bool isDead = false;
+
+  public bool IsDead()
+  {
+    return isDead;
+  }
+
   public void TakeDamage(float damage)
   {
     BroadcastMessage("OnDamageTaken");
     hitPoints -= damage;
-    if (hitPoints <= 0)
+    if (hitPoints <= 0 && !isDead)
     {
-      Destroy(gameObject);
+      Death();
     }
+  }
+
+  void Death()
+  {
+    if (isDead) { return; }
+    isDead = true;
+    GetComponent<Animator>().SetTrigger("dead");
   }
 }
